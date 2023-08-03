f7a1948390b246537560f665d205f5185813d5df
Harikrishnan <harikrishnankpb@gmail.com>
2023-08-03 12:43:58 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://harikrishnankpb.github.io"
+    site: 'https://harikrishnankpb.github.io'
 });
